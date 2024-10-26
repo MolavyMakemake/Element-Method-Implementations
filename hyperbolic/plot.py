e178ce35c49bd42e18470c3e182469b0198c873d
molavymakemake <molavy.business@gmail.com>
2024-10-26 12:22:41 +0200

    .

---
+++ hyperbolic/plot.py
@@ -0,0 +1,69 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import colors
+from matplotlib import tri
+from matplotlib.collections import LineCollection
+from mpl_toolkits.mplot3d.art3d import Line3DCollection
+
+alpha_gradiant = {
+    'alpha': [(0, 0, 0.8),
+            (0.7, 0.3, 0.3),
+            (1, 0.5, 0)],
+    'red': [(0, 0, 0),
+            (1, 1, 0)],
+    'green': [(0, 0, 0),
+            (1, 1, 0)],
+    'blue': [(0, 0, 0),
+            (1, 1, 0)]
+}
+
+alpha_cm = colors.LinearSegmentedColormap('my_colormap2',alpha_gradiant,256)
+
+def complex(ax, vertices, triangles, u):
+    tr = tri.Triangulation(vertices[0, :], vertices[1, :], triangles)
+    arg = np.angle(u) / np.pi + 1
+    arg = np.minimum(arg, 2 - arg)
+
+    shade = np.abs(u)
+    shade = (shade - np.min(shade)) / max((np.max(shade) - np.min(shade), 1e-15))
+    shade = np.log2(shade + 1)
+    #shade = 5 * np.log2(shade) % 1
+
+
+    ax.tricontourf(tr, arg, levels=50, vmin=0, vmax=1, cmap='hsv')
+    ax.tricontourf(tr, shade, levels=30, vmin=0, vmax=1, cmap=alpha_cm)
+    #ax.set(xlim=(-1, 1), ylim=(-1, 1), xlabel='X', ylabel='Y')
+    ax.set_aspect("equal")
+
+def surface(ax, vertices, triangles, u):
+    ax.plot_trisurf(vertices[0, :], vertices[1, :], triangles, u, cmap="Blues")
+    ax.set(xlabel='X', ylabel='Y')
+    #ax.set_aspect("equal")
+
+
+def add_wireframe(ax, X, Y, polygons, u=None):
+    lines = []
+    if u is None:
+        for p_i in polygons:
+            p = np.array([np.take(X, p_i + [p_i[0]]), np.take(Y, p_i + [p_i[0]])]).T
+            lines.append(p)
+
+        ax.add_collection(LineCollection(lines, linewidths=0.2, edgecolors="black"))
+
+    else:
+        for p_i in polygons:
+            p = np.array([np.take(X, p_i + [p_i[0]]), np.take(Y, p_i + [p_i[0]]), np.take(u, p_i + [p_i[0]])]).T
+            lines.append(p)
+
+        ax.add_collection(Line3DCollection(lines, linewidths=0.5, edgecolors="black"))
+
+
+def save(fig, id):
+    ax = fig.axes[0]
+
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    fig.savefig(f"{id.replace(' ', '-').replace('*', '¤')}.jpg", bbox_inches='tight', pad_inches=0, format="jpg")
+
+    ax.get_xaxis().set_visible(True)
+    ax.get_yaxis().set_visible(True)
--- hyperbolic/triangulate.py
+++ hyperbolic/triangulate.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import plot
 
 def radius(p, q):
     a = np.tan(np.pi * (0.5 - 1.0 / q))
@@ -7,15 +8,57 @@ def radius(p, q):
     return np.sqrt((a - b) / (a + b))
 
 # must satisfy (p - 2) * (q - 2) > 4
-p = 3
-q = 7
+p = 5
+q = 5
 
 r = radius(p, q)
 
-angle = np.linspace(0, 2 * np.pi * (1 - 1 / p), p)
-X = r * np.cos(angle)
-Y = r * np.sin(angle)
+angle = 2 * np.pi / p
+X = [r * np.cos((k + 0.5) * angle) for k in range(p)]
+Y = [r * np.sin((k + 0.5) * angle) for k in range(p)]
+triangles = [[i for i in range(p)]]
+flags = [[False for i in range(p)]]
 
-plt.scatter(X, Y)
+l0 = 0.5 / np.cos(0.5 * angle) * (r + 1 / r)
+r0 = l0 * l0 - 1
+
+for _ in range(5):
+    N = len(triangles)
+    c = 0
+
+    for k in range(p):
+        m1 = l0 * np.cos(k * angle)
+        m2 = l0 * np.sin(k * angle)
+
+        for i in range(0, N):
+            if flags[i][k]:
+                continue
+
+            poly = []
+            for j in range(p):
+                u1 = X[triangles[i][j]] - m1
+                u2 = Y[triangles[i][j]] - m2
+                s = r0 / (u1 * u1 + u2 * u2)
+
+                X.append(m1 + s * u1)
+                Y.append(m2 + s * u2)
+                poly.append(3 * N + c + j)
+
+            triangles.append(poly)
+            flags.append([_i == k for _i in range(p)])
+            flags[i][k] = True
+            c += p
+
+def pdisk_to_bkdisk(x, y):
+    s = 0.5 * (1 + x * x + y * y)
+    return x / s, y / s
+
+X, Y = pdisk_to_bkdisk(np.array(X), np.array(Y))
+
+print(len(triangles))
+
+ax = plt.figure().add_subplot()
+plot.add_wireframe(ax, X, Y, triangles)
+plt.scatter(X, Y, s=5)
 plt.axis("equal")
 plt.show()
